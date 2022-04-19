a2d1482f6135b656375551ff877695a5d01eac3f
WooriHCho <woori.helena.cho@gmail.com>
2022-04-19 15:15:55 +0900

    main & booklist page

--- project/BeAwriter/views/auth_views.py
+++ project/BeAwriter/views/auth_views.py
@@ -1,9 +1,12 @@
+from email.policy import default
 from flask import Blueprint, render_template, url_for, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from BeAwriter import db
 from BeAwriter.models import *
+
+from BeAwriter.models import Storybook, Rating
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
--- project/BeAwriter/views/main_views.py
+++ project/BeAwriter/views/main_views.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, render_template, url_for
+from email.policy import default
+from flask import Blueprint, render_template, url_for, request, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
+
+from BeAwriter import db
 from BeAwriter.models import *
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
+    return 'main!'
+
+@bp.route('/main')
+def main():
     return render_template('main/main.html')
