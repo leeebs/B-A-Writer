a7d3d788382396c10a4d3e16262f1371f3b186d4
WooriHCho <woori.helena.cho@gmail.com>
2022-04-21 16:29:41 +0900

    카테고리 성공! 기부니좋아

--- project/BeAwriter/views/main_views.py
+++ project/BeAwriter/views/main_views.py
@@ -18,7 +18,7 @@ def index():
     book_mem_name = []
     for book in book_list:
         member = Member.query.get(book.member_no)
-        rating = Rating.query.get(book.book_no)
+        # rating = Rating.query.get(book.book_no)
         book_mem_name.append(member.member_name)
 
     book_list = book_list.paginate(page, per_page=3)
@@ -51,28 +51,34 @@ def datelist():
     book_list = book_list.paginate(page, per_page=10)
     return render_template('main/datelist.html', book_list=book_list, page=page, book_mem_name=book_mem_name)
 
-# # 별점
-# @bp.route('/starlist')
-# def starlist():
-#     page = request.args.get('page', type=int, default=1)
-#     star_list = Rating.query.order_by(Rating.rating.desc())
-#     book_mem_name = []
-#     book_title = []
-#     book_date = []
-#     book_no
-#     for book in star_list:
-#         member = Member.query.get(book.member_no)
-#         book = Storybook.query.get(book.member_no)
-#         book_mem_name.append(member.member_name)
-#         book_title.append(book.book_title)
-#         book_date.append(book.book_date)
-#         book_no = book.book_no
+# 별점
+@bp.route('/starlist')
+def starlist():
+    page = request.args.get('page', type=int, default=1)
+    star_list = Rating.query.order_by(Rating.rating.desc())
+    book_mem_name = []
+    book_title = []
+    book_date = []
+    book_no
+    for book in star_list:
+        member = Member.query.get(book.member_no)
+        book = Storybook.query.get(book.member_no)
+        book_mem_name.append(member.member_name)
+        book_title.append(book.book_title)
+        book_date.append(book.book_date)
+        book_no = book.book_no
 
-#     star_list = star_list.paginate(page, per_page=10)
-#     return render_template('main/starlist.html', star_list=star_list, page=page, book_mem_name=book_mem_name, book_title=book_title, book_date=book_date, book_no=book_no)
+    star_list = star_list.paginate(page, per_page=10)
+    return render_template('main/starlist.html', star_list=star_list, page=page, book_mem_name=book_mem_name, book_title=book_title, book_date=book_date, book_no=book_no)
 
-# # 내가 만든
-# @bp.route('/mylist', methods=['GET', 'POST'])
-# def mylist():
-#     page = request.args.get('page', type=int, default=1)
-#     return render_template('main/mylist.html', book_list=book_list, page=page, book_mem_name=book_mem_name)
+# 내가 만든
+@bp.route('/mylist', methods=['GET', 'POST'])
+def mylist():
+    page = request.args.get('page', type=int, default=1)
+    book_mem_name = []
+    my_list = Storybook.query.group_by(Storybook.member_no).where(Storybook.member_no == g.user.member_no)
+    for me in my_list:
+        member = Member.query.get(me.member_no)
+        book_mem_name.append(member.member_name)
+    my_list = my_list.paginate(page, per_page=10)
+    return render_template('main/mylist.html', my_list=my_list, page=page, book_mem_name=book_mem_name)
