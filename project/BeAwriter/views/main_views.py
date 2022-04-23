136e9a1924f9e0d096e4d3d383f1f51517143274
WooriHCho <woori.helena.cho@gmail.com>
2022-04-23 13:23:03 +0900

    완성 다했다.

--- project/BeAwriter/views/main_views.py
+++ project/BeAwriter/views/main_views.py
@@ -18,10 +18,13 @@ def index():
     page = request.args.get('page', type=int, default=1)
     book_list = Storybook.query.order_by(Storybook.book_date.desc())
     book_mem_name = []
+    book_avg = []
     for book in book_list:
         member = Member.query.get(book.member_no)
+        rati = Rating.query.get(book.member_no)
         book_mem_name.append(member.member_name)
-    book_list = book_list.paginate(page, per_page=3)
+        book_avg.append(book.avg)
+    book_list = book_list.paginate(page, per_page=4)
 
 
     star_mem_name = []
@@ -37,9 +40,9 @@ def index():
         star_book_date.append(book.book_date)
         star_rate.append(book.avg)
 
-    star_list = star_list.paginate(page, per_page=3)
+    star_list = star_list.paginate(page, per_page=4)
 
-    return render_template('main/main.html', book_list=book_list, page = page, book_mem_name = book_mem_name, 
+    return render_template('main/main.html', book_list=book_list, page = page, book_mem_name = book_mem_name, book_rate=book_avg, 
         star_list=star_list, star_mem_name=star_mem_name, star_book_title=star_book_title, star_book_date=star_book_date, star_rate=star_rate)
    
 
@@ -49,12 +52,14 @@ def datelist():
     page = request.args.get('page', type=int, default=1)
     book_list = Storybook.query.order_by(Storybook.book_date.asc())
     book_mem_name = []
-
+    book_avg = []
     for book in book_list:
         member = Member.query.get(book.member_no)
+        rati = Rating.query.get(book.book_no)
         book_mem_name.append(member.member_name)
-    book_list = book_list.paginate(page, per_page=9)
-    return render_template('main/datelist.html', book_list=book_list, page=page, book_mem_name=book_mem_name)
+        book_avg.append(book.avg)
+    book_list = book_list.paginate(page, per_page=12)
+    return render_template('main/datelist.html', book_list=book_list, page=page, book_mem_name=book_mem_name, book_rate=book_avg)
 
 # 별점
 @bp.route('/starlist')
@@ -62,14 +67,16 @@ def starlist():
     page = request.args.get('page', type=int, default=1)
     star_list = Storybook.query.order_by(Storybook.avg.desc())
     star_mem_name = []
+    star_rate = []
 
     for star in star_list:
         member = Member.query.get(star.member_no)
         star_mem_name.append(member.member_name)
+        star_rate.append(star.avg)
 
-    star_list = star_list.paginate(page, per_page=9)
+    star_list = star_list.paginate(page, per_page=12)
 
-    return render_template('main/starlist.html', star_list=star_list, star_mem_name=star_mem_name)
+    return render_template('main/starlist.html', star_list=star_list, star_mem_name=star_mem_name, star_rate=star_rate)
 
 # 내가 만든
 @bp.route('/mylist', methods=['GET', 'POST'])
@@ -77,13 +84,33 @@ def mylist():
     page = request.args.get('page', type=int, default=1)
     book_mem_name = []
     my_list = Storybook.query.filter(Storybook.member_no == g.user.member_no).order_by(Storybook.book_no.asc())
+    book_rate = []
     for me in my_list:
         member = Member.query.get(me.member_no)
         book_mem_name.append(member.member_name)
-    my_list = my_list.paginate(page, per_page=9)
+        book_rate.append(me.avg)
+    my_list = my_list.paginate(page, per_page=12)
 
-    if my_list:
-        total = Rating.query.with_entities(func.avg(Rating.rating))\
-        .group_by(Rating.book_no).first()[0]
+    return render_template('main/mylist.html', my_list=my_list, page=page, book_mem_name=book_mem_name, book_rate=book_rate)
 
-    return render_template('main/mylist.html', my_list=my_list, page=page, book_mem_name=book_mem_name, rate=total)
+
+# 내가 별점 준 전체
+@bp.route('/mystarlist', methods=['GET', 'POST'])
+def mystarlist():
+    page = request.args.get('page', type=int, default=1)
+    star_mem_name = []
+    star_book_title=[]
+    star_book_date=[]
+    star_rate=[]
+    star_list = Rating.query.filter(Rating.member_no == g.user.member_no).order_by(Rating.book_no.asc())
+    for star in star_list:
+        member = Member.query.get(star.member_no)
+        book = Storybook.query.get(star.book_no)
+        star_mem_name.append(member.member_name)
+        star_book_title.append(book.book_title)
+        star_book_date.append(book.book_date)
+        star_rate.append(book.avg)
+
+    star_list = star_list.paginate(page, per_page=12)
+    return render_template('main/mystarlist.html', star_list=star_list, page=page, star_mem_name=star_mem_name, star_book_title=star_book_title,
+        star_book_date=star_book_date, star_rate=star_rate)
