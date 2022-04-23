dc7d7d3682e1ddbed04d9726e42c000498d47c6e
WooriHCho <woori.helena.cho@gmail.com>
2022-04-23 19:25:23 +0900

    오류 수정

--- project/BeAwriter/views/main_views.py
+++ project/BeAwriter/views/main_views.py
@@ -18,34 +18,40 @@ def index():
     page = request.args.get('page', type=int, default=1)
     book_list = Storybook.query.order_by(Storybook.book_date.desc())
     book_mem_name = []
+    book_img_path = []
     book_avg = []
     for book in book_list:
         member = Member.query.get(book.member_no)
-        rati = Rating.query.get(book.member_no)
         book_mem_name.append(member.member_name)
+        image = Image.query.get(book.book_no)
+        if image:
+            book_img_path.append(image.img_path)
+        else:
+            book_img_path.append(None)
         book_avg.append(book.avg)
+            
     book_list = book_list.paginate(page, per_page=4)
-
-
+    
     star_mem_name = []
-    star_book_title=[]
-    star_book_date=[]
-    star_rate=[]
-    star_list = Rating.query.group_by(Rating.book_no)
+    star_img_path = []
+    star_avg=[]
+
+    star_list = Storybook.query.order_by(Storybook.avg.desc())
     for star in star_list:
         member = Member.query.get(star.member_no)
-        book = Storybook.query.get(star.book_no)
         star_mem_name.append(member.member_name)
-        star_book_title.append(book.book_title)
-        star_book_date.append(book.book_date)
-        star_rate.append(book.avg)
+        image = Image.query.get(book.book_no)
+        if image:
+            star_img_path.append(image.img_path)
+        else:
+            star_img_path.append(None)
+        star_avg.append(star.avg)
 
     star_list = star_list.paginate(page, per_page=4)
 
-    return render_template('main/main.html', book_list=book_list, page = page, book_mem_name = book_mem_name, book_rate=book_avg, 
-        star_list=star_list, star_mem_name=star_mem_name, star_book_title=star_book_title, star_book_date=star_book_date, star_rate=star_rate)
-   
-
+    return render_template('main/main.html', book_list=book_list, page = page, book_mem_name = book_mem_name, book_avg=book_avg,book_img_path=book_img_path,
+        star_list=star_list, star_mem_name=star_mem_name, star_avg=star_avg)
+    
 # 기본 화면 _ 오래된 순으로 표시
 @bp.route('/datelist')
 def datelist():
@@ -55,10 +61,9 @@ def datelist():
     book_avg = []
     for book in book_list:
         member = Member.query.get(book.member_no)
-        rati = Rating.query.get(book.book_no)
         book_mem_name.append(member.member_name)
         book_avg.append(book.avg)
-    book_list = book_list.paginate(page, per_page=12)
+    book_list = book_list.paginate(page, per_page=9)
     return render_template('main/datelist.html', book_list=book_list, page=page, book_mem_name=book_mem_name, book_rate=book_avg)
 
 # 별점
@@ -68,10 +73,16 @@ def starlist():
     star_list = Storybook.query.order_by(Storybook.avg.desc())
     star_mem_name = []
     star_rate = []
+    star_book_title = []
+    star_book_date = []
+    book_avg = []
 
     for star in star_list:
         member = Member.query.get(star.member_no)
         star_mem_name.append(member.member_name)
+        star_book_title.append(star.book_title)
+        star_book_date.append(star.book_date)
+        book_avg.append(star.avg)
         star_rate.append(star.avg)
 
     star_list = star_list.paginate(page, per_page=12)
